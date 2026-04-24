564ef23c16cbfcf670542a0233fffb3cf6c7c035
未来之窗软件服务中心 <ymmygrh@126.com>
2026-04-24 22:34:22 +0800

    Create cyber_appext_config.js

---
+++ app/src/main/assets/app/cyber_appext_config.js
@@ -0,0 +1,576 @@
+ function setSoftVer(ver){
+
+	 
+		switch (ver) {
+		  case "shop_android":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn//o2o/cyberwin_offline_store.php?g=MerchantV6&c=CyberWin_Store&a=Store_cashier_Qujing_android1000x650","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+			//shop_android_phone
+			 case "shop_android_phone":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn//o2o/cyberwin_offline_store.php?g=MerchantV6&c=CyberWin_Store&a=Store_cashier_Qujing_phone","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+			case "shop_cashier":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn//o2o/cyberwin_offline_store.php?g=MerchantV6&c=CyberWin_Store&a=Store_cashier_ClientPConlyPay","20225578-2019");
+			   alert("设置完成，请重启");
+			   CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+			//cashier_Industry_beauty
+			case "cashier_Industry_beauty":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn//o2o/cyberwin_offline_store.php?g=MerchantV6&c=CyberWin_Store&a=Store_cashier_Industry_beauty","20225578-2019");
+			   alert("设置完成，请重启");
+			   CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+			//企业版
+			case "EnterpriseEditionMember":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn//o2o/cyberwin_offline_store.php?g=MerchantV6&c=CyberWin_Store&a=Store_cashier_EnterpriseEditionMember","20225578-2019");
+			   alert("设置完成，请重启");
+			   CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+
+			case "shop_face_onlypay":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn//o2o/cyberwin_offline_store.php?g=MerchantV6&c=CyberWin_Store&a=Store_cashier_Qujing_onlyPay","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+
+			case "dish_normal":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn/o2o/store.php?g=Merchant&c=Store&a=cyberwin_dishDesktop","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+			//平板点餐
+			//dish_platform
+			case "dish_queue":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn/o2o/store.php?g=Merchant&c=Store&a=cyberwin_wlzc_client_queue","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+			case "dish_platform":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn/o2o/store.php?g=Merchant&c=Store&a=cyberwin_dishDesktop_platform","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+			case "pay_store_order":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn/o2o/store.php?g=Merchant&c=Store&a=store_order","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+			case "trade_wuye":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn/o2o/wuye.php","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+
+			//排队叫号
+			//物业管理
+			//   string url = "https://51.onelink.ynwlzc.cn/o2o/store.php?g=Merchant&c=Store&a=cyberwin_wlzc_client_queue";
+			// string url = "https://51.onelink.ynwlzc.cn/o2o/store.php?g=Merchant&c=Store&a=store_order";
+
+
+
+			case "dish_fast":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn/o2o/store.php?g=Merchant&c=Store&a=cyberwin_dish_fastfoodmode","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+			//dish_fast_frogs7
+			case "dish_fast_frogs7":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn/o2o/store.php?g=Merchant&c=Store&a=cyberwin_dish_fastfoodmode_frogs7","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+			//dish_fast_frogs7x400
+			case "dish_fast_frogs7x400":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn/o2o/store.php?g=Merchant&c=Store&a=cyberwin_dish_fastfoodmode_frogs7x400","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+
+			//shop_android_frogs7
+			case "shop_android_frogs7":
+			{
+			   CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn//o2o/cyberwin_offline_store.php?g=MerchantV6&c=CyberWin_Store&a=Store_cashier_Qujing_frogs7","20225578-2019");
+			  alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+
+			case "testpay":
+			{
+				Cyber_JsPrinterStandard.cyber_net_test('https://51.onelink.ynwlzc.cn/o2o/cwpd_pay_pos.cwas/clienttype/andrid','POST','{"appid":"cwpd_pos","auth_code":"13694664664646848","device_sn":"merchant_vir_sn_77","messgae":"","out_trade_no":"157460657086594520191124224206","pay_action":"create","pay_type":"cyberwincloudpay","price":"1","version":"2"}');
+			}
+			break;
+
+			case "testlogin":
+			{
+				Cyber_JsPrinterStandard.cyber_showLogin();
+			}
+			break;
+
+			case "payscanonly":
+			{
+				 CyberWin_JsStandardPlug.cwpd_system_set("APP","show_window_payonly","1","20225578-2019");
+				  alert("设置完成，请重启");
+				   CyberWin_JsStandardPlug.cwpdAppExit();
+
+			}
+			break;
+			case "payscanonly_close":
+			{
+				 CyberWin_JsStandardPlug.cwpd_system_set("APP","show_window_payonly","0","20225578-2019");
+				  CyberWin_JsStandardPlug.cwpdAppExit();
+				  alert("设置完成，请重启");
+				   CyberWin_JsStandardPlug.cwpdAppExit();
+
+			}
+			break;
+			//payscanonly_close
+			//cyber_openhome
+			case "cyberopenhome":
+			{
+				 CyberWin_JsStandardPlug.cyber_openhome();
+
+			}
+			break;
+
+			case "exit":
+			{
+			 
+				   CyberWin_JsStandardPlug.cwpdAppExit();
+
+			}
+			break;
+			//phone_qr
+			case "phone_qr":
+			{
+			 
+				  window.location.href="wlzc_cwpd_qr.html";
+
+			}
+			break;
+
+			//hardware
+			case "hardware":
+			{
+			 
+				  window.location.href="wlzc_cwpd_hardware.html";
+
+			}
+			break;
+			//software_ver
+           case "software_ver":
+			{
+			 
+				  window.location.href="wlzc_cwpd_software_ver.html";
+
+			}
+			break;
+
+				case "test_print_net":
+			{
+			 
+
+			 //  alert($("#ip").val());
+				  //window.location.href="wlzc_cwpd_qr.html";
+				  var ip_s=$("#ip").val();
+				  var port_s="9100";
+				  var data_s=$("#printdata").val();
+
+				   Cyber_JsPrinterStandard.cyber_print("","test",ip_s,"net");
+
+			}
+			break;
+
+			case "test_print_net2":
+			{
+			 
+
+			 //  alert($("#ip").val());
+				  //window.location.href="wlzc_cwpd_qr.html";
+				  var ip_s=$("#ip").val();
+				  var printdata_ss=$("#printdata_s").val();
+				  
+				  var port_s="9100";
+				  var data_s="<?xml version=\"1.0\"?><root><print_type>结账单</print_type><shopname>未来之窗软件服务中心</shopname><order_id>rrrrrrrr</order_id><orderid>rrrrrrrr</orderid><order_no>单号：9200328</order_no><bill_time>2019年11月29日 17:39</bill_time><beizhu>备注</beizhu><clerk_name> </clerk_name><clerk_id> </clerk_id><order_detail> "+printdata_ss+"</order_detail></root>";
+
+				   Cyber_JsPrinterStandard.cyber_print("",data_s,ip_s,"net");
+
+			}
+			break;
+
+			//test_print_net_tmp
+			case "test_print_net_tmp":
+			{
+			 
+
+			 //  alert($("#ip").val());
+				  //window.location.href="wlzc_cwpd_qr.html";
+				  var ip_s=$("#ip").val();
+				  var printdata_ss=$("#printdata_s").val();
+				  
+				  var port_s="9100";
+				  var data_s="<?xml version=\"1.0\"?><root><print_type>结账单</print_type><shopname>未来之窗软件服务中心</shopname><order_id>rrrrrrrr</order_id><orderid>22222222</orderid><order_no>单号：9200328</order_no><bill_time>2019年11月29日 17:39</bill_time><beizhu>备注</beizhu><clerk_name> </clerk_name><clerk_id> </clerk_id><order_detail> "+printdata_ss+"</order_detail></root>";
+
+				   Cyber_JsPrinterStandard.cyber_print("test",data_s,ip_s,"net");
+
+			}
+			break;
+
+
+
+
+			//test_print_usb
+
+			case "test_print_usb":
+			{
+			 
+
+			 //  alert($("#ip").val());
+				  //window.location.href="wlzc_cwpd_qr.html";
+			
+				  var data_s=$("#printdata").val();
+				  var data_ss =  "<?xml version=\"1.0\"?><root><print_type>结账单</print_type><shopname>未来之窗店</shopname><order_id>rrrrrrrr</order_id><orderid>rrrrrrrr</orderid><order_no>单号：P17021500003</order_no><bill_time>2016-04-26 02:30:49</bill_time><beizhu>备注</beizhu><clerk_name>11</clerk_name><clerk_id>11</clerk_id><order_detail>"+data_s+"中碗米线    5    8.0     40.0 \n花生   6\n南瓜饼</order_detail></root>";
+
+				   Cyber_JsPrinterStandard.print_byprintername("",data_ss,"");
+
+			}
+			break;
+
+			case "test_print_blue":
+			{
+			 
+
+			 //  alert($("#ip").val());
+				  //window.location.href="wlzc_cwpd_qr.html";
+				    var data_s=$("#printdata").val();
+				  var data_ss =  "<?xml version=\"1.0\"?><root><print_type>结账单</print_type><shopname>未来之窗店</shopname><order_id>rrrrrrrr</order_id><orderid>rrrrrrrr</orderid><order_no>单号：P17021500003</order_no><bill_time>2016-04-26 02:30:49</bill_time><beizhu>备注</beizhu><clerk_name>11</clerk_name><clerk_id>11</clerk_id><order_detail>"+data_s+"中碗米线    5    8.0     40.0 \n花生   6\n南瓜饼</order_detail></root>";
+
+				   Cyber_JsPrinterStandard.print_byprintername("",data_ss,"");
+
+			}
+			break;
+
+				case "test_print_select":
+			{
+			 
+
+			 
+
+				   Cyber_JsPrinterStandard.cyber_PrinterLinkModeChoose();
+
+			}
+			break;
+
+			case "wlzc_cwpd_frame_main":
+			{
+			 
+				  window.location.href="wlzc_cwpd_frame_main.html";
+
+			}
+			break;
+
+			//test_print_net
+
+			//btn_back
+			case "btn_back":
+			{
+			 
+				  top.location.href="wlzc_cwpd_login.html";
+
+			}
+			break;
+
+				case "setDefault_usb":
+			{
+			 
+
+			 
+				  var cwpdusbdevice=$("#cwpdusbdevices").val();
+				 
+				   CyberWin_JsStandardPlug.cwpd_system_set("printer","default",cwpdusbdevice,"20225578-2019");
+				   var d= CyberWin_JsStandardPlug.cwpd_system_get("printer","default","20225578-2019");
+	                alert("当前打印机：:"+d);
+
+			}
+			break;
+
+			//设置打印设备类型
+			case "setprinter_type":
+			{
+			 
+
+			 
+				  var cwpdusbdevice_type=$("#cwpdusbdevices_type").val();
+				 
+				   CyberWin_JsStandardPlug.cwpd_system_set("printer","devicetype",cwpdusbdevice_type,"20225578-2019");
+				   var d= CyberWin_JsStandardPlug.cwpd_system_get("printer","devicetype","20225578-2019");
+	                alert("当前打印机：:"+d);
+
+			}
+			break;
+
+			//
+				//设置打印设备纸张类型
+			case "cwpdprint_report_width":
+			{
+			 
+
+			 
+				  var cwpdusbdevice_type=$("#cwpdprint_report_width").val();
+				 
+				   CyberWin_JsStandardPlug.cwpd_system_set("printer","report_width",cwpdusbdevice_type,"20225578-2019");
+				   var d= CyberWin_JsStandardPlug.cwpd_system_get("printer","report_width","20225578-2019");
+	                alert("当前打印机：:"+d);
+
+			}
+			break;
+
+			//设置打网络打印
+			case "cwpdusbdevices_net":
+			{
+			 
+
+			 
+				  var cwpdusbdevice_net=$("#cwpdusbdevices_net").val();
+				 
+				   CyberWin_JsStandardPlug.cwpd_system_set("printer","net",cwpdusbdevice_net,"20225578-2019");
+				   var d= CyberWin_JsStandardPlug.cwpd_system_get("printer","net","20225578-2019");
+	                alert("当前打印机：:"+d);
+
+			}
+			break;
+
+			//
+
+				case "getUsbDevice":
+			{
+			 
+
+			 
+				 // var cwpdusbdevice=$("#cwpdusbdevices").val();
+				 
+				  var devices_str= CyberWin_JsStandardPlug.cwpd_Device_getUsbDeviceList();
+				  var devices_array= devices_str.split("|");
+				  for (i = 0; i < devices_array.length; i++) { 
+					  // document.write(devices_array[i] + "<br />");
+					  $("#cwpdusbdevices").append("<option value='"+devices_array[i]+"'>"+devices_array[i]+"</option>");
+				  }
+
+			}
+			break;
+
+			 
+				case "shop_android_member":
+			{
+			 
+
+			 
+				 // var cwpdusbdevice=$("#cwpdusbdevices").val();
+				 
+				  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn//o2o/cyberwin_offline_store.php?g=MerchantV6&c=CyberWin_Store&a=Store_cashier_MemberClient","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+
+			}
+			break;
+			//print_newall
+				case "print_newall":
+			{
+			 
+              var print_tmp=$("#print_tmp").val();
+			  var data_s=$("#printdata").val();
+			   var iotype=$("#print_interface").val();
+			  var address=$("#ip").val();
+				  var data_ss =  "<?xml version=\"1.0\"?><root><print_type>结账单</print_type><shopname>未来之窗店"+iotype+"</shopname><order_id>rrrrrrrr</order_id><orderid>rrrrrrrr</orderid><order_no>单号：P17021500003</order_no><bill_time>2016-04-26 02:30:49</bill_time><beizhu>备注</beizhu><clerk_name>11</clerk_name><clerk_id>11</clerk_id><order_detail>"+data_s+"中碗米线    5    8.0     40.0 \n花生   6\n南瓜饼</order_detail></root>";
+
+				  // Cyber_JsPrinterStandard.print_byprintername("",data_ss,"");
+				  //  Cyber_JsPrinterStandard.cyber_print("","",data_ss,"192.168.1.198",iotype);
+					//cyber_printV3Common
+					  Cyber_JsPrinterStandard.cyber_printV3Common(print_tmp,data_ss,address,iotype);
+
+			
+	      }
+		break;
+
+				case "print_settmp":
+			{
+			 
+               var print_tmp=$("#print_tmp").val();
+		 
+			 
+			  var print_tmp_data=$("#print_tmp_data").val();
+				 
+				Cyber_JsPrinterStandard.cwpd_system_config_report_set("commonreport",print_tmp,print_tmp_data,"20225578-2019");
+
+			
+	      }
+			break;
+
+			//print_gettmp
+					case "print_gettmp":
+			{
+			 
+               var print_tmp=$("#print_tmp").val();
+		 
+			 
+			  
+				 
+			var tmp=	Cyber_JsPrinterStandard.cwpd_system_config_report_get("commonreport",print_tmp,"20225578-2019");
+			$("#print_tmp_data").val(tmp);
+
+			
+	      }
+			break;
+			case "get_serial":
+			{
+			 
+             
+		 
+			 
+			  
+				 
+			 Cyber_JsPrinterStandard.cyber_getSerialData();
+		 
+			
+	      }
+			break;
+			//本地存储
+
+			case "cwpdusbdevices_test_localstoraget":
+			{
+			 
+
+			 //  alert($("#ip").val());
+				  //window.location.href="wlzc_cwpd_qr.html";
+			
+				  var cwpd_test_localstrogae=$("#cwpd_test_localstrogae").val();
+
+				   var r1=Cyber_JsPrinterStandard.locStorage_getVal("cyberwinphpdefault","local_cache","testwlzc");
+				  alert(r1);
+				 
+				  CyberWin_JsStandardPlug.locStorage_setVal("cyberwinphpdefault","local_cache","testwlzc", cwpd_test_localstrogae);
+				  var r=CyberWin_JsStandardPlug.locStorage_getVal("cyberwinphpdefault","local_cache","testwlzc");
+				  alert(r);
+
+			}
+			break;
+
+
+			//设置屏幕方向
+			case "cwpddevices_setscreen_Orientation":
+			{
+			 
+
+			 //  alert($("#ip").val());
+				  //window.location.href="wlzc_cwpd_qr.html";
+			
+				  var cwpddevices_Orientation=$("#cwpddevices_Orientation").val();
+
+				  
+				   
+				 
+				   CyberWin_JsStandardPlug.cwpd_system_set("Device","ScreenOrientation",cwpddevices_Orientation,"20225578-2019");
+				   var d= CyberWin_JsStandardPlug.cwpd_system_get("Device","ScreenOrientation","20225578-2019");
+	                alert("屏幕方向：:"+d);
+
+			}
+			break;
+
+			//设置启动模式
+			case "cwpddevices_setstart_mode":
+			{
+			 
+
+			 //  alert($("#ip").val());
+				  //window.location.href="wlzc_cwpd_qr.html";
+			
+				  var cwpddevices_Orientation=$("#start_mode").val();
+
+				  
+				   
+				 
+				   CyberWin_JsStandardPlug.cwpd_system_set("APP","start_mode",cwpddevices_Orientation,"20225578-2019");
+				   var d= CyberWin_JsStandardPlug.cwpd_system_get("APP","start_mode","20225578-2019");
+	                alert("启动模式：:"+d);
+
+			}
+			break;
+
+
+			//快餐8800x
+				case "dish_fast_800":
+			{
+			  CyberWin_JsStandardPlug.cwpd_system_set("APP","DefaultHome","https://51.onelink.ynwlzc.cn/o2o/store.php?g=Merchant&c=Store&a=cyberwin_dish_fastfoodmode&screen_mode=pos800","20225578-2019");
+			   alert("设置完成，请重启");
+			    CyberWin_JsStandardPlug.cwpdAppExit();
+			}
+			break;
+
+			//设置启动模式
+			case "cwpddevices_setdpi_mode":
+			{
+			 
+
+			 //  alert($("#ip").val());
+				  //window.location.href="wlzc_cwpd_qr.html";
+			
+				  var cwpddevices_Orientation=$("#dpi_mode").val();
+
+				  
+				   
+				 
+				   CyberWin_JsStandardPlug.cwpd_system_set("APP","WebZoom",cwpddevices_Orientation,"20225578-2019");
+				   var d= CyberWin_JsStandardPlug.cwpd_system_get("APP","WebZoom","20225578-2019");
+	                alert("dpi模式：:"+d);
+
+			}
+			break;
+
+
+			//get_serial
+			default:
+				 break;
+     }
+
+ 
+}
+
+ function setSoftConfigV2(sec,key,value,title){
+	   
+
+	 CyberWin_JsStandardPlug.cwpd_system_set(sec,key,value,"20225578-2019");
+	  var d= CyberWin_JsStandardPlug.cwpd_system_get(sec,key,"20225578-2019");
+	  alert("设置成功当前：:"+title);
+ }
